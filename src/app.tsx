60111e6476d36671fcb2c345d6a4905c94248747
Maicon Lourenço <maiconloure@gmail.com>
2024-02-07 20:04:44 -0300

    feat: improvements in note cards and added new toast

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from './assets/logo-nlw-expert.svg';
 import { NewNoteCard } from './components/new-note-card';
 import { NoteCard } from './components/note-card';
 
+const note = {
+  date: new Date(2023, 4, 1),
+  content: 'Hello, World',
+}
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -19,9 +24,7 @@ export function App() {
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
       </div>
     </div>
   )
